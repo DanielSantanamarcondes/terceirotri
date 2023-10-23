d74920769dde2cf732ce8ee65ba4de9fd29159eb
DanielSantanamarcondes <127766044+DanielSantanamarcondes@users.noreply.github.com>
2023-10-23 12:26:48 -0300

    Add files via upload

--- mains.js
+++ mains.js
@@ -7,15 +7,18 @@ document.querySelector(idElementoAudio).play();
 
         const listaDeTeclas = document.querySelectorA11('.tecla');
 
-      let contador = 0;
+      
        
-        while(contador <listaDeTeclas.length) {
-        const tecla = listaDeTeclas[contador];
+        for(let contador = 0;contador < listaDeTeclas.length;contador++) {
+            const tecla = listaDeTeclas[contador];
             const instrumento = tecla.classLista[1];
-            const idAudio = '#som_ $(instrumento)';
-           console.log(idAudio);
-           tecla.onclick = function (){
-                tocaSom(idAudio);
-            };
-            contador = contador + 1;
+            const idAudio = `#som_ ${instrumento}`;
+            console.log(idAudio);
+
+            tecla.onclick = function (){
+               tocaSom(idAudio);  
         }
+            tecla.onkeydown = fuction(){
+              
+            }
+      }
